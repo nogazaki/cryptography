4430c8d2653d470226f0746b79495fea42e203c5
nogazaki <tarutojido@gmail.com>
2023-10-29 00:11:37 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cryptography
+
+me learning cryptography algorithms
+
+## :warning: Security Warning :warning:
+
+Theses implementation is for learning purposes and should not be used in anything serious.
---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+//! Cryptography algorithms
+//!
+//! # Security Warning
+//!
+//! Theses implementations are for learning purposes and should not be used in anything serious.
+
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
+// no_std support is not on by default
+#![cfg_attr(feature = "no_std", no_std)]
+
+/// Error code definitions
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ErrorCode {
+    /// A wrong key type was provided
+    WrongKeyType,
+    /// An argument did not pass the check
+    InvalidArgument,
+    /// Provided pointer to receive data is not large enough
+    InsufficientMemory,
+    /// Unknown error code
+    #[default]
+    Unknown,
+}
